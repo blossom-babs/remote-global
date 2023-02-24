7c0cfb4fec7244c818f6360b997a5581cfe0e803
Blossom <saphashb@gmail.com>
2023-02-24 21:33:27 +0100

    Edit about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,19 +1,15 @@
 import Link from 'next/link';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import ReportIcon from '@mui/icons-material/Report';
 
 const About = () => {
-	const formRef = useRef(null);
 	const [success, setSuccess] = useState(false);
 
 	const handleSubmit = (e: { preventDefault: () => void }) => {
 		e.preventDefault();
 		const formElement = document.querySelector('#form') as HTMLFormElement;
 		const formData = new FormData(formElement);
-
-		console.log(formData);
 
 		fetch(
 			'https://script.google.com/macros/s/AKfycbxAJjPI6YorvCvj_sWg-nK6gF0SYZjVWT5_-nmDrfNzPfoz6GYzAAf_74HqBWlldFHN/exec',
@@ -33,24 +29,29 @@ const About = () => {
 		<div className="about">
 			<div className="about-main">
 				<h1 className="">
-					Connecting African Talents to International opportunities hiring
-					globally.
+					Connecting African Talents to International companies hiring globally.
 				</h1>
 			</div>
 
 			<div className="about-whatWeDo">
-				<h2>❌ What this is not ❌</h2>
+				<h2>❌ What this is Not ❌</h2>
 
 				<div className="about-whatWeDo-box">
 					<div className="about-whatWeDo-card">
-						<h3>Not An Agency</h3>
+						<div>
+							<ReportIcon />
+						</div>
+						<h3>An Agency</h3>
 						<p>
 							This is not an Agency for recruiting talents to jobs or companies.
 							We have no affiliations to the companies shared.
 						</p>
 					</div>
 					<div className="about-whatWeDo-card">
-						<h3>Not A Job board</h3>
+						<div>
+							<ReportIcon />
+						</div>
+						<h3>A Job board</h3>
 						<p>
 							This website contains little information about whether the
 							companies are currently hiring or the roles they are hiring for.
@@ -62,15 +63,16 @@ const About = () => {
 
 			<div className="about-why">
 				<p>
-					We exist to make your job search easier by curating a board of
+					We exist to make your job search easier by curating a list of
 					companies known for hiring talents globally.
 				</p>
+				<h3>Know a company hiring globally?</h3>
 				<p className="about-why-pitch">
-					<Link href="/pitch">Pitch a Company </Link> to be addded to the board.
+					<Link href="/pitch">Pitch them </Link>
 				</p>
 			</div>
 
-			<div className="about-footer">
+			<div id="join-the-waitlist" className="about-footer">
 				<h2>Stay in touch...</h2>
 				<p>
 					Be the first to get updates about international companies looking to
@@ -105,7 +107,7 @@ const About = () => {
 
 export const SuccessMessage = () => {
 	return (
-		<div>
+		<div className="about-whatWeDo-card about-success">
 			<div>
 				<CheckCircleIcon />
 			</div>

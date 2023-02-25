56f6755f6429e12624f4467819bc23ca642e7be1
Blossom <saphashb@gmail.com>
2023-02-25 12:05:25 +0100

    revert next.config.js settings

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
-
-module.exports = {
-  reactsStrictMode: true,
-
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  }
+const nextConfig = {
+  reactStrictMode: true,
 }
+
+module.exports = nextConfig

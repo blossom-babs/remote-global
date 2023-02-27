ffb9a654c3dd632814db95f1e56740db9a932e45
Blossom <saphashb@gmail.com>
2023-02-27 22:55:08 +0100

    create filter and display header

--- src/pages/companies.tsx
+++ src/pages/companies.tsx
@@ -1,9 +1,11 @@
 import Layout from '@/components/Layout';
 import Waitlist from '@/components/Waitlist';
+import MultipleSelectChip from '@/components/MultiSelectComp';
 import Head from 'next/head';
 import Link from 'next/link';
 import { useState } from 'react';
-
+import TableRowsIcon from '@mui/icons-material/TableRows';
+import GridViewIcon from '@mui/icons-material/GridView';
 export default function Companies() {
 	// 2 kind of views - grid and list
 	return (
@@ -14,9 +16,17 @@ export default function Companies() {
 			<>
 				<Layout>
 					<main className="companies">
-            <div className="companies-waitlist">
-						<Waitlist />
-            </div>
+						<div className="companies-waitlist">
+							<header className='companies-header'>
+								<MultipleSelectChip />
+
+								<div>
+<GridViewIcon/>
+									<TableRowsIcon/>
+								</div>
+							</header>
+							<Waitlist />
+						</div>
 					</main>
 				</Layout>
 			</>

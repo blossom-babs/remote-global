94a1334ba4717120f3ea38d212d3af01f71a4aa1
Blossom <saphashb@gmail.com>
2023-03-07 12:21:13 +0100

    create company dataset

---
+++ data/companies.ts
@@ -0,0 +1,55 @@
+export const companies = [
+	{
+		companyName: 'Aha!',
+		companyLogo:
+			'https://media.glassdoor.com/sql/1102346/aha-labs-squarelogo-1447427190779.png',
+		website: 'https://www.aha.io/',
+		about: 'Aha! helps companies build lovable software.',
+		companySize: '51 - 200',
+		approved: true,
+		location: 'Menlo Park, CA'
+	},
+	{
+		companyName: 'Airbyte',
+		companyLogo:
+			'https://media.glassdoor.com/sql/7651014/airbyte-ca-squarelogo-1663228063571.png',
+		website: 'https://www.aha.io/',
+		about:
+			'Airbyte is an open-source data integration engine that helps you consolidate your data in your data warehouses, lakes and databases.',
+		companySize: '51 - 200',
+		approved: true,
+		location: 'San Francisco, CA'
+	},
+	{
+		companyName: '10up',
+		companyLogo:
+			'https://media.glassdoor.com/sql/775906/10up-squarelogo-1400949368497.png',
+		website: 'https://10up.com/',
+		about:
+			'We make the web better by finely crafting websites & tools for content creators.',
+		companySize: '201 - 500',
+		approved: true,
+		location: 'Roseville, CA'
+	},
+	{
+		companyName: '17hats',
+		companyLogo:
+			'https://media.glassdoor.com/sql/1327368/17hats-squarelogo-1499404803292.png',
+		website: 'https://www.17hats.com/',
+		about: 'To help service-driven, small business owners succeed.',
+		companySize: '1 - 50',
+		approved: true,
+		location: 'Pasadena, CA'
+	},
+	{
+		companyName: 'Acquia',
+		companyLogo:
+			'https://media.glassdoor.com/sql/347852/acquia-squareLogo-1630515493739.png',
+		website: 'https://www.acquia.com/',
+		about:
+			'Acquia is driving the open-source revolution as the leading provider of cloud-based digital experience management solutions.',
+		companySize: '1001 - 5000',
+		approved: true,
+		location: 'Boston, MA'
+	}
+];

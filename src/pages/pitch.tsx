b5d1a37293bad84cec82226ab8d6263958c6b15f
Blossom <saphashb@gmail.com>
2023-03-11 23:21:06 +0100

    set up boilerplate for pitch page

--- src/pages/pitch.tsx
+++ src/pages/pitch.tsx
@@ -1,3 +1,19 @@
-export default function Pitch(){
+import Layout from '@/components/Layout';
+import Head from 'next/head';
 
+export default function Pitch() {
+	return (
+		<>
+			<Head>
+				<title>Pitch a company hiring globally</title>
+			</Head>
+			<>
+				<Layout>
+					<div className="pitch">
+						<h1>Pitch a global company</h1>
+					</div>
+				</Layout>
+			</>
+		</>
+	);
 }

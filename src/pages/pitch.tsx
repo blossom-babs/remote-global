b7f3f01a267770ef125928f5e03b196fca5e4954
Blossom <saphashb@gmail.com>
2023-03-13 22:50:00 +0100

    add form templates to pitch and signin

--- src/pages/pitch.tsx
+++ src/pages/pitch.tsx
@@ -11,6 +11,37 @@ export default function Pitch() {
 				<Layout>
 					<div className="pitch">
 						<h1>Pitch a global company</h1>
+						<div className="about-footer-form">
+					<form onSubmit={handleSubmit} id="form" method="post" action="">
+						<div>
+							<input
+								onChange={() => setError(false)}
+								type="text"
+								name="FirstName"
+								placeholder="First Name"
+							/>
+						</div>
+						<div>
+							<input
+								onChange={() => setError(false)}
+								type="email"
+								name="Email"
+								placeholder="Enter Your email"
+							/>
+						</div>
+						<div>
+							<button onClick={clickSubmit} type="submit">
+								Join the Waitlist
+							</button>
+						</div>
+					</form>
+					{loading && <CircularProgress disableShrink color="inherit" />}
+					{error && (
+						<p style={{ color: 'red' }}>
+							Error Submitting Form! Please try again.
+						</p>
+					)}
+				</div>
 					</div>
 				</Layout>
 			</>
--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,5 +1,35 @@
 // this is a protected route
 
 export default function Signin(){
-  
+  <div className="about-footer-form">
+  <form onSubmit={handleSubmit} id="form" method="post" action="">
+    <div>
+      <input
+        onChange={() => setError(false)}
+        type="text"
+        name="FirstName"
+        placeholder="First Name"
+      />
+    </div>
+    <div>
+      <input
+        onChange={() => setError(false)}
+        type="email"
+        name="Email"
+        placeholder="Enter Your email"
+      />
+    </div>
+    <div>
+      <button onClick={clickSubmit} type="submit">
+        Join the Waitlist
+      </button>
+    </div>
+  </form>
+  {loading && <CircularProgress disableShrink color="inherit" />}
+  {error && (
+    <p style={{ color: 'red' }}>
+      Error Submitting Form! Please try again.
+    </p>
+  )}
+</div> 
 }

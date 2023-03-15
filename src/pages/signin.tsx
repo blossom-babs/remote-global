08924793b50785afd8e3b5473c247fa5fa5fd863
Blossom <saphashb@gmail.com>
2023-03-15 18:08:14 +0100

    update signin page

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,35 +1,38 @@
 // this is a protected route
+import { useState } from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
 
-export default function Signin(){
-  <div className="about-footer-form">
-  <form onSubmit={handleSubmit} id="form" method="post" action="">
-    <div>
-      <input
-        onChange={() => setError(false)}
-        type="text"
-        name="FirstName"
-        placeholder="First Name"
-      />
-    </div>
-    <div>
-      <input
-        onChange={() => setError(false)}
-        type="email"
-        name="Email"
-        placeholder="Enter Your email"
-      />
-    </div>
-    <div>
-      <button onClick={clickSubmit} type="submit">
-        Join the Waitlist
-      </button>
-    </div>
-  </form>
-  {loading && <CircularProgress disableShrink color="inherit" />}
-  {error && (
-    <p style={{ color: 'red' }}>
-      Error Submitting Form! Please try again.
-    </p>
-  )}
-</div> 
+export default function Signin() {
+	const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+	<div className="about-footer-form">
+		<form id="form" method="post" action="">
+			<div>
+				<input
+					onChange={() => setError(false)}
+					type="text"
+					name="FirstName"
+					placeholder="First Name"
+				/>
+			</div>
+			<div>
+				<input
+					onChange={() => setError(false)}
+					type="email"
+					name="Email"
+					placeholder="Enter Your email"
+				/>
+			</div>
+			<div>
+				<button type="submit">
+					Join the Waitlist
+				</button>
+			</div>
+		</form>
+		{loading && <CircularProgress disableShrink color="inherit" />}
+		{error && (
+			<p style={{ color: 'red' }}>Error Submitting Form! Please try again.</p>
+		)}
+	</div>;
 }

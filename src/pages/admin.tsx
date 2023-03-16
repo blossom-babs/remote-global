244be44e27baf1b2e7c7ce4eee0b885d3b6ef69b
Blossom <saphashb@gmail.com>
2023-03-16 17:25:48 +0100

    create table and modal in admin page

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,64 +1,162 @@
-// import Layout from '@/components/Layout';
-// import * as React from 'react';
-// import { DataGrid, GridColDef } from '@mui/x-data-grid';
-// import { prisma } from 'lib/prisma';
+import Layout from '@/components/Layout';
+import { useEffect, useState } from 'react';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { prisma } from 'lib/prisma';
+import Image from 'next/image';
+import Link from 'next/link';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import Dialog from '@mui/material/Dialog';
+import { ButtonUnstyled } from '@mui/base';
 
-// const columns: GridColDef[] = [
-// 	{ field: 'companyLogo', headerName: 'Logo', width: 130 },
-// 	{ field: 'companyName', headerName: 'Name', width: 130 },
-// 	{ field: 'location', headerName: 'location', width: 130 },
-// 	{ field: 'size', headerName: 'size', width: 90 }
-// ];
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
-// interface Companies {
-// 	companies: {
-// 		id: string;
-// 		companyName: string;
-// 		companyLogo: string;
-// 		website: string;
-// 		about: string;
-// 		companySize: string;
-// 		approved: boolean;
-// 		location: string;
-// 	}[];
-// }
+interface Companies {
+	companies: {
+		id: string;
+		companyName: string;
+		companyLogo: string;
+		website: string;
+		about: string;
+		companySize: string;
+		approved: boolean;
+		location: string;
+	}[];
+}
 
-// export default function Admin({ companies }: Companies) {
-// 	return (
-// 		<>
-// 			<Layout>
-// 				<div className="admin">
-// 					<h1>this is a protected route</h1>
+export default function Admin({ companies }: Companies) {
+	const [data, setData] = useState(null);
+	const [open, setOpen] = useState(false);
 
-// 					<div style={{ height: 400, width: '100%' }}>
-// 						<DataGrid
-// 							rows={}
-// 							columns={columns}
-// 							pageSize={5}
-// 							rowsPerPageOptions={[5]}
-// 							checkboxSelection
-// 						/>
-// 					</div>
-// 				</div>
-// 			</Layout>
-// 		</>
-// 	);
-// }
+	const handleClickOpen = (id: string) => {
+		setOpen(true);
+		localStorage.setItem('company', id);
+	};
 
-// export async function getServerSideProps() {
-// 	const companies = await prisma.company.findMany();
+	const handleClose = () => {
+		setOpen(false);
+	};
 
-// 	return {
-// 		props: {
-// 			companies: JSON.parse(JSON.stringify(companies))
-// 		}
-// 	};
-// }
-
-export default function Admin() {
+	//useEffect(() => setData(companies), [])
 	return (
 		<>
-			<h1>the header page</h1>
+			<Layout>
+				<div className="admin">
+					<>{open && <ModalBox open={open} handleClose={handleClose} />}</>
+					<div style={{ overflowX: 'auto' }}>
+						<table className="table">
+							<thead>
+								<tr className="table-header">
+									<th>Company</th>
+									<th>Website</th>
+									<th className="table-size">Size</th>
+									<th>Location</th>
+									<th></th>
+								</tr>
+							</thead>
+
+							<tbody>
+								{companies.map((data) => (
+									<tr key={data.id} className="table-companyList">
+										<td>
+											<img
+												src={data.companyLogo}
+												width={30}
+												height={30}
+												alt={data.companyName}
+											/>
+											<p> {data.companyName}</p>
+										</td>
+										<td className="table-link">
+											<Link
+												href={data.website}
+												target="_blank"
+												rel="noopener noreffer">
+												{data.website}
+												<span>
+													<OpenInNewIcon />
+												</span>
+											</Link>
+										</td>
+										<td className="table-size">{data.companySize}</td>
+										<td>{data.location}</td>
+										<td>
+											<button onClick={() => handleClickOpen(data.id)}>
+												<RemoveRedEyeIcon />
+											</button>
+										</td>
+									</tr>
+								))}
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</Layout>
 		</>
 	);
+}
+
+export const ModalBox = (props: any) => {
+	const [company, setCompany] = useState();
+
+	const companyId = localStorage.getItem('company') as string;
+
+	async function getNote(id: string) {
+		try {
+			fetch(`http://localhost:3000/api/company/${id}`, {
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				method: 'GET'
+			})
+				.then((response) => response.json())
+				.then((data) => {
+					setCompany(data)
+				});
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	useEffect(() => {
+		getNote(companyId);
+	}, []);
+
+	const compId = localStorage.getItem('company');
+	console.log(compId);
+	return (
+		<Dialog
+			open={props.open}
+			onClose={props.handleClose}
+			aria-labelledby="alert-dialog-title"
+			aria-describedby="alert-dialog-description">
+			<DialogTitle id="alert-dialog-title">
+				{"Use Google's location service?"}
+			</DialogTitle>
+			<DialogContent>
+				<DialogContentText id="alert-dialog-description">
+					{company && company['companyName']}
+				</DialogContentText>
+			</DialogContent>
+			<DialogActions>
+				<button onClick={props.handleClose}>Disagree</button>
+				<button onClick={props.handleClose} autoFocus>
+					Agree
+				</button>
+			</DialogActions>
+		</Dialog>
+	);
+};
+
+export async function getServerSideProps() {
+	const companies = await prisma.company.findMany();
+
+	return {
+		props: {
+			companies: JSON.parse(JSON.stringify(companies))
+		}
+	};
 }

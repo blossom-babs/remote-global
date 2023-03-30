ec4ced4a0055ae2887061132e35f8f7e0179b215
Blossom <saphashb@gmail.com>
2023-03-30 13:51:29 +0100

    enable analytics in app.tsx folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '../styles/index.scss';
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Component {...pageProps} />
+			<Analytics />
+		</>
+	);
 }
